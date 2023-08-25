9db74f86827a43257a44604ebe2100a201e6b184
ngdymx <ngyumx@163.com>
2023-08-25 10:32:27 -0400

    correct

--- kernel_src/layer_buffer_hbm00.cpp
+++ kernel_src/layer_buffer_hbm00.cpp
@@ -1,29 +1,28 @@
 #include "hls_task.h"
 #include "ap_axi_sdata.h"
 #include "hls_burst_maxi.h"
-#include "./fdtd_types.hpp"
-#include "./infs_types.hpp"
+#include "./types.hpp"
 #define HBM_SIZE 0x10000000
 #define HBM_OFFSET 0x00000000
 
-void cell_p2s(hls::stream<fdtd_cell_pack>& fdtd_cell_pack_stream_in, hls::stream<fdtd_cell>& cell_stream_out){
+void cell_p2s(hls::stream<data_cell_pack>& data_cell_pack_stream_in, hls::stream<data_cell>& cell_stream_out){
 #pragma HLS interface ap_ctrl_none port = return
 #pragma HLS PIPELINE style=flp II=1
-    static fdtd_cell_pack i_temp;
+    static data_cell_pack i_temp;
     static ap_uint<3> counter = 0;
     static bool inited=false;
 
     if(!inited){
         counter = 0;
-        if(!fdtd_cell_pack_stream_in.empty()){
+        if(!data_cell_pack_stream_in.empty()){
             inited = true;
         }
     }
     else{
         if (counter == 0){
-            fdtd_cell_pack_stream_in >> i_temp;
+            data_cell_pack_stream_in >> i_temp;
         }
-        fdtd_cell o_temp;
+        data_cell o_temp;
         o_temp = i_temp.data[counter];
         cell_stream_out << o_temp;
         if(counter == 3){
@@ -35,10 +34,10 @@ void cell_p2s(hls::stream<fdtd_cell_pack>& fdtd_cell_pack_stream_in, hls::stream
     }
 }
 
-void cell_s2p(hls::stream<fdtd_cell>& cell_stream_in, hls::stream<fdtd_cell_pack>& fdtd_cell_pack_stream_out){
+void cell_s2p(hls::stream<data_cell>& cell_stream_in, hls::stream<data_cell_pack>& data_cell_pack_stream_out){
 #pragma HLS interface ap_ctrl_none port = return
 #pragma HLS PIPELINE style=flp II=1
-    static fdtd_cell_pack o_temp;
+    static data_cell_pack o_temp;
     static ap_uint<3> counter = 0;
     static bool inited=false;
 
@@ -53,11 +52,11 @@ void cell_s2p(hls::stream<fdtd_cell>& cell_stream_in, hls::stream<fdtd_cell_pack
         }
     }
     else{
-        fdtd_cell i_temp;
+        data_cell i_temp;
         cell_stream_in >> i_temp;
         o_temp.data[counter] = i_temp;
         if(counter == 3){
-            fdtd_cell_pack_stream_out << o_temp;
+            data_cell_pack_stream_out << o_temp;
         }
         if(counter == 3){
             counter = 0;
@@ -67,7 +66,7 @@ void cell_s2p(hls::stream<fdtd_cell>& cell_stream_in, hls::stream<fdtd_cell_pack
         }
     }
 }
-void write2HBM(hls::burst_maxi<fdtd_cell_pack> mem, hls::stream<fdtd_cell_pack>& stream_in, hls::stream<ap_uint<1>>& flag_stream_out){
+void write2HBM(hls::burst_maxi<data_cell_pack> mem, hls::stream<data_cell_pack>& stream_in, hls::stream<ap_uint<1>>& flag_stream_out){
 #pragma HLS INTERFACE ap_ctrl_none port = return
 
     for(int count = 0; count < (HBM_SIZE >> 12); count++){
@@ -76,7 +75,7 @@ void write2HBM(hls::burst_maxi<fdtd_cell_pack> mem, hls::stream<fdtd_cell_pack>&
         write_burst_loop:
             for(int i = 0; i < 64; i++){
 #pragma HLS PIPELINE 
-                fdtd_cell_pack i_temp;
+                data_cell_pack i_temp;
                 stream_in >> i_temp;
                 mem.write(i_temp);
             }
@@ -85,7 +84,7 @@ void write2HBM(hls::burst_maxi<fdtd_cell_pack> mem, hls::stream<fdtd_cell_pack>&
         }
 }
 
-void readfromHBM(hls::burst_maxi<fdtd_cell_pack> mem, hls::stream<ap_uint<1>>& flag_stream_in, hls::stream<fdtd_cell_pack>& stream_out){
+void readfromHBM(hls::burst_maxi<data_cell_pack> mem, hls::stream<ap_uint<1>>& flag_stream_in, hls::stream<data_cell_pack>& stream_out){
 #pragma HLS INTERFACE ap_ctrl_none port = return
     for(int count = 0; count < (HBM_SIZE >> 12); count++){
 #pragma HLS PIPELINE off
@@ -96,7 +95,7 @@ void readfromHBM(hls::burst_maxi<fdtd_cell_pack> mem, hls::stream<ap_uint<1>>& f
 read_burst_loop:
             for(int i = 0; i < 64; i++){
 #pragma HLS PIPELINE 
-                fdtd_cell_pack i_temp;
+                data_cell_pack i_temp;
                 i_temp = mem.read();
                 stream_out << i_temp;
             }
@@ -107,15 +106,15 @@ read_burst_loop:
 
 extern "C" {
 
-void layer_buffer_hbm00(hls::burst_maxi<fdtd_cell_pack> mem1, hls::burst_maxi<fdtd_cell_pack> mem2, hls::stream<fdtd_cell>& stream_in, hls::stream<fdtd_cell>& stream_out){
+void layer_buffer_hbm00(hls::burst_maxi<data_cell_pack> mem1, hls::burst_maxi<data_cell_pack> mem2, hls::stream<data_cell>& stream_in, hls::stream<data_cell>& stream_out){
 #pragma HLS INTERFACE ap_ctrl_none port = return
 #pragma HLS INTERFACE m_axi port=mem1 bundle=gmem1 num_read_outstanding=1 num_write_outstanding=4 latency=4 offset=slave
 #pragma HLS INTERFACE m_axi port=mem2 bundle=gmem2 num_read_outstanding=4 num_write_outstanding=1 latency=4 offset=slave
 
 
     hls_thread_local hls::stream<ap_uint<1>, 96> flag_stream_1;
-    hls_thread_local hls::stream<fdtd_cell_pack> pack_stream_out;
-    hls_thread_local hls::stream<fdtd_cell_pack> pack_stream_in;
+    hls_thread_local hls::stream<data_cell_pack> pack_stream_out;
+    hls_thread_local hls::stream<data_cell_pack> pack_stream_in;
 
     hls_thread_local hls::task s2p(cell_s2p, stream_in, pack_stream_out);
     hls_thread_local hls::task inst_s2mm(write2HBM, mem1, pack_stream_out,  flag_stream_1);
